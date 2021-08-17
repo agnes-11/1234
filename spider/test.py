60408b183a84bd1a18f6221104d558e8e6edbc0e
agnes-11 <68411044+agnes-11@users.noreply.github.com>
2021-08-18 00:57:51 +0800

    Update test.py

--- spider/test.py
+++ spider/test.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+driver = webdriver.Chrome(options=options,executable_path=chromedriver)
 driver.get("https://www.baidu.com")
 print(driver.title)
 driver.quit()
