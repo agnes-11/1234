4cf853d16a91092801c3a78bef5885e606191633
agnes-11 <68411044+agnes-11@users.noreply.github.com>
2021-08-18 14:53:05 +0800

    Update test.py

--- spider/test.py
+++ spider/test.py
@@ -9,7 +9,7 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(options=chrome_options,executable_path=chromedriver)
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 driver.get("https://www.baidu.com")
 print(driver.title)
 driver.quit()
